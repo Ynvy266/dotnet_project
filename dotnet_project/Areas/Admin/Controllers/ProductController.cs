1bd5e123bf257b688d95e1adfb1ad42897ec1e73
YnVy <nguyenyenvy266@gmail.com>
2024-11-28 01:30:24 +0700

    Edit & Delete product.

--- dotnet_project/Areas/Admin/Controllers/ProductController.cs
+++ dotnet_project/Areas/Admin/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using dotnet_project.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace dotnet_project.Areas.Admin.Controllers
 {
@@ -80,6 +81,84 @@ namespace dotnet_project.Areas.Admin.Controllers
             }
 
             return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            ProductModel product = await _dataContext.Products.FindAsync(Id);
+            ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name", product.CategoryId);
+            ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name", product.BrandId);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, ProductModel product)
+        {
+            ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name", product.CategoryId);
+            ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name", product.BrandId);
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.Replace(" ", "-");
+                var slug = await _dataContext.Products.FirstOrDefaultAsync(p => p.Slug == product.Slug); //Find product by slug
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product is already available in the database.");
+                    return View(product);
+                }
+
+                if (product.ImageUpload != null)
+                {
+                    string uploadDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
+                    string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadDir, imageName);
+
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                    product.Image = imageName;
+                }
+                _dataContext.Update(product);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Successfully updated the product information!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Model has some errors.";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            ProductModel product = await _dataContext.Products.FindAsync(Id);
+            if (!string.Equals(product.Image, "noname.jpg"))
+            {
+                string uploadDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
+                string oldFileImage = Path.Combine(uploadDir, product.Image);
+                if (System.IO.File.Exists(oldFileImage)) 
+                { 
+                    System.IO.File.Delete(oldFileImage);
+                }
+            }
+            _dataContext.Products.Remove(product);
+            await _dataContext.SaveChangesAsync();
+            TempData["error"] = "Successfully deleted the product!";
+            return RedirectToAction("Index");
         }
     }
 }
