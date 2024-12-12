83ebc8c3ff65f9c02fd6b6ab55c1ea0f7e26f4b0
hoaavii <vithihoa3132003@gmail.com>
2024-12-12 15:49:24 +0700

    statistic by filter-option

--- dotnet_project/Areas/Admin/Controllers/DashboardController.cs
+++ dotnet_project/Areas/Admin/Controllers/DashboardController.cs
@@ -30,9 +30,53 @@ namespace dotnet_project.Areas.Admin.Controllers
 			return View();
         }
         [HttpPost]
-        public async Task<IActionResult> GetChartData()
+		[Route("GetChartData")]
+		public async Task<IActionResult> GetChartData()
         {
             var data = _dataContext.Statistic
+                .Select(s => new
+                {
+                    date = s.DateCreated.ToString("yyyy-MM-dd"),
+                    sold = s.Sold,
+                    quantity = s.Quantity,
+                    revenue = s.Revenue,
+                    profit = s.Profit,
+                })
+                .ToList();
+            return Json(data);
+        }
+        [HttpPost]
+        [Route("GetChartDataBySelect")]
+        public IActionResult GetChartDataBySelect(DateTime startDate, DateTime endDate)
+        {
+            var data = _dataContext.Statistic
+                .Where(s => s.DateCreated >= startDate && s.DateCreated <= endDate)
+                .Select(s => new
+                {
+                    date = s.DateCreated.ToString("yyyy-MM-dd"),
+                    sold = s.Sold,
+                    quantity = s.Quantity,
+                    revenue = s.Revenue,
+                    profit = s.Profit,
+                })
+                .ToList();
+            return Json(data);
+        }
+        [HttpPost]
+        [Route("FilterData")]
+        public IActionResult FilterData(DateTime? fromDate, DateTime? toDate)
+        {
+            var query = _dataContext.Statistic.AsQueryable();
+            if(fromDate.HasValue)
+            {
+                query = query.Where(s => s.DateCreated >= fromDate);
+            }
+            if(toDate.HasValue)
+            {
+                query = query.Where(s => s.DateCreated <= toDate);
+            }
+            
+            var data = query
                 .Select(s => new
                 {
                     date = s.DateCreated.ToString("yyyy-MM-dd"),
---
+++ dotnet_project/Migrations/20241212084416_dotnet1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_project.Migrations
+{
+    public partial class dotnet1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoleId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Statistic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Sold = table.Column<int>(type: "int", nullable: false),
+                    Revenue = table.Column<int>(type: "int", nullable: false),
+                    Profit = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistic", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Statistic");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "AspNetUsers");
+        }
+    }
+}
